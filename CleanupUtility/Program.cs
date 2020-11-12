4063c2d8e41bbee8ec25af9792bf454a184ac787
tss-awertheimer <root@DESKTOP-G1L1POL.trafficsafetystore.local>
2020-11-12 10:58:23 -0500

    Initial Commit

---
+++ CleanupUtility/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CleanupUtility
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			//Connect to server/directory
+			//Crawl files and for old files
+
+		}
+	}
+}
